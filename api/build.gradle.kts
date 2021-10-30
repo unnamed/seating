e807f5431cde5a28e215d107baa16dafb2811066
pixeldev <amiranda8695@gmail.com>
2021-10-30 13:12:31 -0500

    feat(adapt): adding base adaption for 1.8

---
+++ api/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+    api("org.jetbrains:annotations:22.0.0")
+    compileOnly("org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT")
+}
---
+++ runtime/adapt-v1_8_R3/src/main/java/team/unnamed/chairs/adapt/v1_8_R3/chair/ChairUtils.java
@@ -0,0 +1,66 @@
+package team.unnamed.chairs.adapt.v1_8_R3.chair;
+
+import net.minecraft.server.v1_8_R3.EntityArmorStand;
+import net.minecraft.server.v1_8_R3.PacketPlayOutAttachEntity;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving;
+
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import team.unnamed.chairs.ChairData;
+import team.unnamed.chairs.EntitySpectators;
+import team.unnamed.chairs.adapt.v1_8_R3.Packets;
+
+public final class ChairUtils {
+
+    private ChairUtils() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int generateId(ChairData chairData) {
+        EntityArmorStand armorStand = new EntityArmorStand(
+                ((CraftWorld) chairData.getLocation().getWorld()).getHandle()
+        );
+
+        return armorStand.getId();
+    }
+
+    public static void destroy(ChairData chairData, Player spectator) {
+        chairData.getSpectators().removeSpectator(spectator);
+        Packets.send(spectator, new PacketPlayOutEntityDestroy(chairData.getEntityId()));
+    }
+
+    public static void spawn(ChairData chairData, Player spectator) {
+        EntitySpectators spectators = chairData.getSpectators();
+
+        if (spectators.isSpectator(spectator)) {
+            return;
+        }
+
+        Location location = chairData.getLocation();
+        EntityArmorStand armorStand = new EntityArmorStand(
+                ((CraftWorld) location.getWorld()).getHandle()
+        );
+
+        armorStand.setLocation(
+                location.getX(), location.getY(), location.getZ(),
+                location.getYaw(), location.getPitch()
+        );
+
+        armorStand.setGravity(false);
+        armorStand.setInvisible(true);
+        armorStand.setSmall(true);
+
+        Packets.send(
+                spectator,
+                new PacketPlayOutSpawnEntityLiving(armorStand),
+                new PacketPlayOutAttachEntity(
+                        0, ((CraftPlayer) chairData.getOwner()).getHandle(), armorStand
+                )
+        );
+    }
+
+}
