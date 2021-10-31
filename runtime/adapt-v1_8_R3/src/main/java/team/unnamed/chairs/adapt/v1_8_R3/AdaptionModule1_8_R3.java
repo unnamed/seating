680fdb37e0dde29ebe042af98125a1575a4a1f36
pixeldev <amiranda8695@gmail.com>
2021-10-31 17:27:18 -0600

    feat(adapt): add methods to get packet interceptor api.

--- runtime/adapt-v1_8_R3/src/main/java/team/unnamed/chairs/adapt/v1_8_R3/AdaptionModule1_8_R3.java
+++ runtime/adapt-v1_8_R3/src/main/java/team/unnamed/chairs/adapt/v1_8_R3/AdaptionModule1_8_R3.java
@@ -1,14 +1,30 @@
 package team.unnamed.chairs.adapt.v1_8_R3;
 
+import team.unnamed.chairs.ChairDataRegistry;
 import team.unnamed.chairs.adapt.AdaptionModule;
 import team.unnamed.chairs.adapt.entity.ChairEntityHandler;
+import team.unnamed.chairs.adapt.intercept.PacketInterceptorAssigner;
+import team.unnamed.chairs.adapt.intercept.PacketInterceptorRegister;
 import team.unnamed.chairs.adapt.v1_8_R3.chair.ChairEntityHandler1_8_R3;
+import team.unnamed.chairs.adapt.v1_8_R3.intercept.PacketInterceptorAssigner1_8_R3;
+import team.unnamed.chairs.adapt.v1_8_R3.intercept.PacketInterceptorRegister1_8_R3;
 
 public class AdaptionModule1_8_R3 implements AdaptionModule {
 
     @Override
     public ChairEntityHandler getChairEntityHandler() {
         return new ChairEntityHandler1_8_R3();
+    }
+
+    @Override
+    public PacketInterceptorRegister getPacketInterceptorRegister(ChairDataRegistry chairDataRegistry,
+                                                                  ChairEntityHandler chairEntityHandler) {
+        return new PacketInterceptorRegister1_8_R3(chairDataRegistry, chairEntityHandler);
+    }
+
+    @Override
+    public PacketInterceptorAssigner getPacketInterceptorAssigner() {
+        return new PacketInterceptorAssigner1_8_R3();
     }
 
 }
--- runtime/src/main/java/team/unnamed/chairs/adapt/AdaptionModule.java
+++ runtime/src/main/java/team/unnamed/chairs/adapt/AdaptionModule.java
@@ -1,9 +1,17 @@
 package team.unnamed.chairs.adapt;
 
+import team.unnamed.chairs.ChairDataRegistry;
 import team.unnamed.chairs.adapt.entity.ChairEntityHandler;
+import team.unnamed.chairs.adapt.intercept.PacketInterceptorAssigner;
+import team.unnamed.chairs.adapt.intercept.PacketInterceptorRegister;
 
 public interface AdaptionModule {
 
     ChairEntityHandler getChairEntityHandler();
+
+    PacketInterceptorRegister getPacketInterceptorRegister(ChairDataRegistry chairDataRegistry,
+                                                           ChairEntityHandler chairEntityHandler);
+
+    PacketInterceptorAssigner getPacketInterceptorAssigner();
 
 }
