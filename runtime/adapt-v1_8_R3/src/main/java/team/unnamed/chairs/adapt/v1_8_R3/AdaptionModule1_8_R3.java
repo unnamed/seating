8b3c5900ddf67a965f37b9af2f425f9f5ac0851d
pixeldev <amiranda8695@gmail.com>
2021-11-01 12:29:12 -0600

    feat: add worldguard hook manager.

--- runtime/adapt-v1_8_R3/src/main/java/team/unnamed/chairs/adapt/v1_8_R3/AdaptionModule1_8_R3.java
+++ runtime/adapt-v1_8_R3/src/main/java/team/unnamed/chairs/adapt/v1_8_R3/AdaptionModule1_8_R3.java
@@ -3,9 +3,11 @@ package team.unnamed.chairs.adapt.v1_8_R3;
 import team.unnamed.chairs.ChairDataRegistry;
 import team.unnamed.chairs.adapt.AdaptionModule;
 import team.unnamed.chairs.adapt.entity.ChairEntityHandler;
+import team.unnamed.chairs.adapt.hook.HookManager;
 import team.unnamed.chairs.adapt.intercept.PacketInterceptorAssigner;
 import team.unnamed.chairs.adapt.intercept.PacketInterceptorRegister;
 import team.unnamed.chairs.adapt.v1_8_R3.chair.ChairEntityHandler1_8_R3;
+import team.unnamed.chairs.adapt.v1_8_R3.hook.WorldGuardHookManager1_8_R3;
 import team.unnamed.chairs.adapt.v1_8_R3.intercept.PacketInterceptorAssigner1_8_R3;
 import team.unnamed.chairs.adapt.v1_8_R3.intercept.PacketInterceptorRegister1_8_R3;
 
@@ -14,6 +16,11 @@ public class AdaptionModule1_8_R3 implements AdaptionModule {
     @Override
     public ChairEntityHandler getChairEntityHandler() {
         return new ChairEntityHandler1_8_R3();
+    }
+
+    @Override
+    public HookManager getWorldGuardHookManager() {
+        return new WorldGuardHookManager1_8_R3();
     }
 
     @Override
--- runtime/src/main/java/team/unnamed/chairs/adapt/AdaptionModule.java
+++ runtime/src/main/java/team/unnamed/chairs/adapt/AdaptionModule.java
@@ -2,12 +2,15 @@ package team.unnamed.chairs.adapt;
 
 import team.unnamed.chairs.ChairDataRegistry;
 import team.unnamed.chairs.adapt.entity.ChairEntityHandler;
+import team.unnamed.chairs.adapt.hook.HookManager;
 import team.unnamed.chairs.adapt.intercept.PacketInterceptorAssigner;
 import team.unnamed.chairs.adapt.intercept.PacketInterceptorRegister;
 
 public interface AdaptionModule {
 
     ChairEntityHandler getChairEntityHandler();
+
+    HookManager getWorldGuardHookManager();
 
     PacketInterceptorRegister getPacketInterceptorRegister(ChairDataRegistry chairDataRegistry,
                                                            ChairEntityHandler chairEntityHandler);
