bf2efff151de4c870c65eb3787b93273b93eec92
pixeldev <amiranda8695@gmail.com>
2021-11-04 23:02:36 -0600

    fix: now get center of armor stand base location

--- api/src/main/java/team/unnamed/chairs/ChairData.java
+++ api/src/main/java/team/unnamed/chairs/ChairData.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.material.Stairs;
 
 import java.util.UUID;
 
@@ -63,15 +64,49 @@ public class ChairData {
 
     public static ChairData create(Player player, Block block) {
         Location location = block.getLocation().clone();
-        double incrementY = -0.5;
+        Material material = block.getType();
+        float yaw = player.getLocation().getYaw();
+        double incrementX = 0.5;
+        double incrementY;
+        double incrementZ = 0.5;
 
-        if (block.getType() == Material.STEP) {
-            incrementY = -1.25;
-        } else if (block.getType() == Material.CARPET) {
-            incrementY = -1.5;
+        if (material == Material.CARPET) {
+            incrementY = 1.5;
+        } else if (material == Material.STEP) {
+            incrementY = 1.25;
+        } else {
+            Stairs stairs = (Stairs) block.getState().getData();
+            switch (stairs.getDescendingDirection()) {
+                case EAST: {
+                    yaw = -90;
+                    incrementX = 0.8;
+                    break;
+                }
+                case WEST: {
+                    yaw = 90;
+                    incrementX = 0.2;
+                    break;
+                }
+                case NORTH: {
+                    yaw = -180;
+                    incrementZ = 0.2;
+                    break;
+                }
+                case SOUTH: {
+                    yaw = 0;
+                    incrementZ = 0.8;
+                    break;
+                }
+            }
+
+            incrementY = 1.3;
         }
 
-        location.add(0.5, incrementY, 0.5);
+        Location playerLocation = player.getLocation();
+        playerLocation.setYaw(yaw);
+        player.teleport(playerLocation);
+        location.add(incrementX, -incrementY, incrementZ);
+        location.setYaw(yaw);
         return new ChairData(player.getUniqueId(), location, player.getLocation());
     }
 
