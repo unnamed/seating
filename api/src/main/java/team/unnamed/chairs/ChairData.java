d504fb4c63df2c1f246449d53e52b8ca579108b6
pixeldev <amiranda8695@gmail.com>
2021-11-08 16:56:09 -0600

    fix: migrate calculate location and using weak reference

--- api/src/main/java/team/unnamed/chairs/ChairData.java
+++ api/src/main/java/team/unnamed/chairs/ChairData.java
@@ -1,36 +1,37 @@
 package team.unnamed.chairs;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.material.Stairs;
 
+import java.lang.ref.WeakReference;
 import java.util.UUID;
 
 public class ChairData {
 
-    private final UUID ownerId;
+    private final WeakReference<Player> ownerReference;
+    private final Material blockType;
     private final Location location;
     private final Location firstLocation;
     private int entityId;
 
     private final EntitySpectators spectators;
 
-    private ChairData(UUID ownerId, Location location, Location firstLocation) {
-        this.ownerId = ownerId;
+    private ChairData(Player owner, Material blockType,
+                      Location location, Location firstLocation) {
+        this.ownerReference = new WeakReference<>(owner);
+        this.blockType = blockType;
         this.location = location;
         this.firstLocation = firstLocation;
         this.spectators = new EntitySpectators();
     }
 
     public Player getOwner() {
-        return Bukkit.getPlayer(ownerId);
+        return ownerReference.get();
     }
 
-    public UUID getOwnerId() {
-        return ownerId;
+    public UUID getOwnerReference() {
+        return getOwner().getUniqueId();
     }
 
     public Location getLocation() {
@@ -49,6 +50,10 @@ public class ChairData {
         this.entityId = entityId;
     }
 
+    public Material getBlockType() {
+        return blockType;
+    }
+
     public EntitySpectators getSpectators() {
         return spectators;
     }
@@ -56,58 +61,14 @@ public class ChairData {
     @Override
     public String toString() {
         return "ChairData{" +
-                "ownerId=" + ownerId +
+                "ownerId=" + ownerReference +
                 ", location=" + location +
                 ", entityId=" + entityId +
                 '}';
     }
 
-    public static ChairData create(Player player, Block block) {
-        Location location = block.getLocation().clone();
-        Material material = block.getType();
-        float yaw = player.getLocation().getYaw();
-        double incrementX = 0.5;
-        double incrementY;
-        double incrementZ = 0.5;
-
-        if (material == Material.CARPET) {
-            incrementY = 1.5;
-        } else if (material == Material.STEP) {
-            incrementY = 1.25;
-        } else {
-            Stairs stairs = (Stairs) block.getState().getData();
-            switch (stairs.getDescendingDirection()) {
-                case EAST: {
-                    yaw = -90;
-                    incrementX = 0.8;
-                    break;
-                }
-                case WEST: {
-                    yaw = 90;
-                    incrementX = 0.2;
-                    break;
-                }
-                case NORTH: {
-                    yaw = -180;
-                    incrementZ = 0.2;
-                    break;
-                }
-                case SOUTH: {
-                    yaw = 0;
-                    incrementZ = 0.8;
-                    break;
-                }
-            }
-
-            incrementY = 1.3;
-        }
-
-        Location playerLocation = player.getLocation();
-        playerLocation.setYaw(yaw);
-        player.teleport(playerLocation);
-        location.add(incrementX, -incrementY, incrementZ);
-        location.setYaw(yaw);
-        return new ChairData(player.getUniqueId(), location, player.getLocation());
+    public static ChairData create(Player player, Material type, Location baseLocation) {
+        return new ChairData(player, type, baseLocation, player.getLocation());
     }
 
 }
