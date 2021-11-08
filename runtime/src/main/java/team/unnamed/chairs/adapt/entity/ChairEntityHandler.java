cc9afef04d0f7c4218c2fea9327a234743306c5e
pixeldev <amiranda8695@gmail.com>
2021-11-08 16:56:47 -0600

    fix: remove unused methods and add calculate base location

--- runtime/src/main/java/team/unnamed/chairs/adapt/entity/ChairEntityHandler.java
+++ runtime/src/main/java/team/unnamed/chairs/adapt/entity/ChairEntityHandler.java
@@ -1,14 +1,14 @@
 package team.unnamed.chairs.adapt.entity;
 
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import team.unnamed.chairs.ChairData;
 
 public interface ChairEntityHandler {
 
-    void showChairMount(ChairData chairData, Player player);
-
-    void updateChairYaw(ChairData chairData);
+    Location calculateBaseLocation(Player owner, Block block);
 
     void assignArmorStand(ChairData chairData);
 
