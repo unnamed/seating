41719aebd1501f43b3937f831e4ef11a47c87fd9
pixeldev <amiranda8695@gmail.com>
2021-11-08 17:01:07 -0600

    deps: add viaversion

--- runtime/adapt-v1_8_R3/build.gradle.kts
+++ runtime/adapt-v1_8_R3/build.gradle.kts
@@ -3,4 +3,5 @@ dependencies {
     compileOnly("com.sk89q.worldguard:worldguard-legacy:6.2")
     compileOnly("com.sk89q.worldedit:worldedit-core:6.1.4-SNAPSHOT")
     compileOnly("io.papermc.paper:paper:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("com.viaversion:viaversion-api:4.0.2-SNAPSHOT")
 }
