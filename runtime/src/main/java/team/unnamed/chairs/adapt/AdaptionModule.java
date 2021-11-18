7d519c6c2eb76c940ae85cf269e7ff15e7f37b77
yusshu <yushu@unnamed.team>
2021-11-18 09:47:28 -0500

    chore: remove unused imports

--- runtime/src/main/java/team/unnamed/chairs/adapt/AdaptionModule.java
+++ runtime/src/main/java/team/unnamed/chairs/adapt/AdaptionModule.java
@@ -1,6 +1,5 @@
 package team.unnamed.chairs.adapt;
 
-import team.unnamed.chairs.ChairDataRegistry;
 import team.unnamed.chairs.adapt.entity.ChairEntityHandler;
 import team.unnamed.chairs.adapt.hook.HookManager;
 import team.unnamed.chairs.adapt.intercept.PacketInterceptorAssigner;
