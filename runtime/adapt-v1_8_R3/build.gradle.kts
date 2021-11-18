03327aa3f7fa49fd4ec16879c624f9b2aed28268
yusshu <yushu@unnamed.team>
2021-11-18 09:20:28 -0500

    deps(v1_8_R3): fix paper dependency, it is under org.github.paperspigot in this version

--- runtime/adapt-v1_8_R3/build.gradle.kts
+++ runtime/adapt-v1_8_R3/build.gradle.kts
@@ -2,6 +2,6 @@ dependencies {
     compileOnly(project(":runtime"))
     compileOnly("com.sk89q.worldguard:worldguard-legacy:6.2")
     compileOnly("com.sk89q.worldedit:worldedit-core:6.1.4-SNAPSHOT")
-    compileOnly("io.papermc.paper:paper:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("org.github.paperspigot:paperspigot:1.8.8-R0.1-SNAPSHOT")
     compileOnly("com.viaversion:viaversion-api:4.0.2-SNAPSHOT")
 }
