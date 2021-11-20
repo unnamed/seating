3b137e59f56e881b51184758ae4f562f52f05dd4
pixeldev <amiranda8695@gmail.com>
2021-11-20 09:15:28 -0600

    refactor: adapt to new api

--- plugin/src/main/java/team/unnamed/seating/listener/PlayerDismountFakeEntityListener.java
+++ plugin/src/main/java/team/unnamed/seating/listener/PlayerDismountFakeEntityListener.java
@@ -3,32 +3,25 @@ package team.unnamed.seating.listener;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
 import team.unnamed.seating.SeatingData;
 import team.unnamed.seating.SeatingDataRegistry;
-import team.unnamed.seating.adapt.entity.SeatingEntityHandler;
 import team.unnamed.seating.event.PlayerDismountFakeEntityEvent;
 
 public class PlayerDismountFakeEntityListener implements Listener {
 
-    private final SeatingEntityHandler seatingEntityHandler;
     private final SeatingDataRegistry seatingDataRegistry;
 
-    public PlayerDismountFakeEntityListener(SeatingEntityHandler seatingEntityHandler,
-                                            SeatingDataRegistry seatingDataRegistry) {
-        this.seatingEntityHandler = seatingEntityHandler;
+    public PlayerDismountFakeEntityListener(SeatingDataRegistry seatingDataRegistry) {
         this.seatingDataRegistry = seatingDataRegistry;
     }
 
     @EventHandler
     public void onDismountFakeEntity(PlayerDismountFakeEntityEvent event) {
         Player player = event.getPlayer();
-        SeatingData seatingData = seatingDataRegistry.getRegistry(player);
+        SeatingData seatingData = seatingDataRegistry.removeRegistry(player);
 
         if (seatingData != null) {
             player.teleport(seatingData.getFirstLocation());
-            seatingEntityHandler.destroy(seatingData);
-            seatingDataRegistry.removeRegistry(player);
         }
     }
 
--- plugin/src/main/java/team/unnamed/seating/listener/PlayerLeaveListener.java
+++ plugin/src/main/java/team/unnamed/seating/listener/PlayerLeaveListener.java
@@ -1,42 +1,27 @@
 package team.unnamed.seating.listener;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-
-import team.unnamed.seating.SeatingData;
 import team.unnamed.seating.SeatingDataRegistry;
-import team.unnamed.seating.adapt.entity.SeatingEntityHandler;
 
 public class PlayerLeaveListener implements Listener {
 
     private final SeatingDataRegistry seatingDataRegistry;
-    private final SeatingEntityHandler seatingEntityHandler;
 
-    public PlayerLeaveListener(SeatingDataRegistry seatingDataRegistry,
-                               SeatingEntityHandler seatingEntityHandler) {
+    public PlayerLeaveListener(SeatingDataRegistry seatingDataRegistry) {
         this.seatingDataRegistry = seatingDataRegistry;
-        this.seatingEntityHandler = seatingEntityHandler;
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        removeSeat(event.getPlayer());
+        seatingDataRegistry.removeRegistry(event.getPlayer());
     }
 
     @EventHandler
     public void onKick(PlayerKickEvent event) {
-        removeSeat(event.getPlayer());
-    }
-
-    private void removeSeat(Player player) {
-        SeatingData seatingData = seatingDataRegistry.getRegistry(player);
-        if (seatingData != null) {
-            seatingEntityHandler.destroy(seatingData);
-            seatingDataRegistry.removeRegistry(player);
-        }
+        seatingDataRegistry.removeRegistry(event.getPlayer());
     }
 
 }
