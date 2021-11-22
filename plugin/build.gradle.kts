84319a2f22d44357dcb003dca307b3b8661ffe86
yusshu <yushu@unnamed.team>
2021-11-22 01:21:45 -0500

    build: include v1_16_R3 and v1_8_R3 if java version is 16 too

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,12 +6,12 @@ dependencies {
     api(project(":adapt"))
     compileOnly("org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT")
 
+
+    arrayOf("1_16_R3", "1_8_R3").forEach {
+        runtimeOnly(project(":adapt:adapt-v$it"))
+    }
     if (project.property("java") == "16") {
         arrayOf("1_17_R1").forEach {
-            runtimeOnly(project(":adapt:adapt-v$it"))
-        }
-    } else {
-        arrayOf("1_16_R3", "1_8_R3").forEach {
             runtimeOnly(project(":adapt:adapt-v$it"))
         }
     }
