9a033db2137539743aa7cf55f085293bb2d51e01
pixeldev <amiranda8695@gmail.com>
2021-11-29 23:17:18 -0600

    refactor: make seating handler an interface for more functions

--- api/src/main/java/team/unnamed/seating/SeatingHandler.java
+++ api/src/main/java/team/unnamed/seating/SeatingHandler.java
@@ -1,72 +1,21 @@
 package team.unnamed.seating;
 
-import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
-import java.util.List;
+public interface SeatingHandler {
 
-import static team.unnamed.bukkit.ServerVersionUtils.SERVER_VERSION_INT;
+    boolean isAllowedToUse(Player player);
 
-public class SeatingHandler {
+    boolean isWorldDenied(World world);
 
-    private final FileConfiguration configuration;
+    boolean isChairMaterial(Block block);
 
-    public SeatingHandler(FileConfiguration configuration) {
-        this.configuration = configuration;
-    }
+    void toggleSeating(Player player);
 
-    public boolean isAllowedToUse(Player player) {
-        return player.hasPermission("seating.interaction");
-    }
+    void sit(Player player, Block block, boolean ignoreType);
 
-    public boolean isWorldDenied(World world) {
-        List<String> deniedWorldNames = configuration.getStringList("seating.denied-worlds");
-        return deniedWorldNames.contains(world.getName());
-    }
-
-    public boolean isAllowedMaterial(Material material) {
-        Object configuredStairs = configuration.get("seating.materials");
-        String materialName = material.name();
-
-        if (configuredStairs instanceof String) {
-            String stairMaterialKey = (String) configuredStairs;
-
-            if (stairMaterialKey.equals("ALL")) {
-                boolean isStair = materialName.contains("STAIRS");
-
-                if (SERVER_VERSION_INT < 13) {
-                    return isStair || material == Material.STEP
-                            || material == Material.WOOD_STEP
-                            || material == Material.CARPET;
-                } else {
-                    return isStair || materialName.contains("SLAB")
-                            || materialName.contains("CARPET");
-                }
-            } else {
-                Material stairMaterial = Material.matchMaterial(stairMaterialKey);
-
-                if (stairMaterial == null) {
-                    return false;
-                } else {
-                    return stairMaterial == material;
-                }
-            }
-        } else if (configuredStairs instanceof List) {
-            @SuppressWarnings("unchecked") List<String> stairsMaterialKeys =
-                    (List<String>) configuredStairs;
-
-            for (String stairMaterialKey : stairsMaterialKeys) {
-                if (materialName.contains(stairMaterialKey)) {
-                    return true;
-                }
-            }
-
-            return false;
-        } else {
-            return false;
-        }
-    }
+    void crawl(Player player);
 
 }
---
+++ plugin/src/main/java/team/unnamed/seating/SimpleSeatingHandler.java
@@ -0,0 +1,155 @@
+package team.unnamed.seating;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import team.unnamed.seating.adapt.hook.HookRegistry;
+import team.unnamed.seating.adapt.material.MaterialChecker;
+import team.unnamed.seating.data.ChairSeatingData;
+import team.unnamed.seating.data.CrawlSeatingData;
+import team.unnamed.seating.message.MessageHandler;
+import team.unnamed.seating.user.UserToggleSeatingManager;
+
+import java.util.List;
+
+public class SimpleSeatingHandler implements SeatingHandler {
+
+    private final FileConfiguration configuration;
+    private final MessageHandler messageHandler;
+    private final SeatingDataRegistry<CrawlSeatingData> crawlDataRegistry;
+    private final SeatingDataRegistry<ChairSeatingData> chairDataRegistry;
+    private final HookRegistry hookRegistry;
+    private final MaterialChecker materialChecker;
+    private final UserToggleSeatingManager userToggleSeatingManager;
+
+    public SimpleSeatingHandler(FileConfiguration configuration,
+                                MessageHandler messageHandler,
+                                SeatingDataRegistry<CrawlSeatingData> crawlDataRegistry,
+                                SeatingDataRegistry<ChairSeatingData> chairDataRegistry,
+                                HookRegistry hookRegistry,
+                                MaterialChecker materialChecker,
+                                UserToggleSeatingManager userToggleSeatingManager) {
+        this.configuration = configuration;
+        this.messageHandler = messageHandler;
+        this.crawlDataRegistry = crawlDataRegistry;
+        this.chairDataRegistry = chairDataRegistry;
+        this.hookRegistry = hookRegistry;
+        this.materialChecker = materialChecker;
+        this.userToggleSeatingManager = userToggleSeatingManager;
+    }
+
+    @Override
+    public boolean isAllowedToUse(Player player) {
+        return player.hasPermission("seating.sit");
+    }
+
+    @Override
+    public boolean isWorldDenied(World world) {
+        List<String> deniedWorldNames = configuration.getStringList("seating.denied-worlds");
+        return deniedWorldNames.contains(world.getName());
+    }
+
+    @Override
+    public boolean isChairMaterial(Block block) {
+        List<String> configuredChairsMaterials
+                = configuration.getStringList("seating.chairs-materials");
+
+        for (String key : configuredChairsMaterials) {
+            String[] tags = key.split("#");
+
+            if (tags.length == 1) {
+                Material material = Material.getMaterial(tags[0]);
+                if (material == block.getType()) {
+                    return true;
+                }
+            } else {
+                switch (tags[1]) {
+                    case "SLABS": {
+                        if (materialChecker.isSlab(block)) {
+                            return true;
+                        }
+                    }
+                    case "STAIRS": {
+                        if (materialChecker.isStair(block)) {
+                            return true;
+                        }
+                    }
+                    case "CARPETS": {
+                        if (materialChecker.isCarpet(block)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void toggleSeating(Player player) {
+        String path = userToggleSeatingManager.toggleSeating(player) ? "enable" : "disable";
+        messageHandler.sendMessage(player, path + "-seatings");
+    }
+
+    @Override
+    public void sit(Player player, Block block, boolean ignoreType) {
+        if (!ignoreType && !isChairMaterial(block)) {
+            return;
+        }
+
+        if (userToggleSeatingManager.hasSeatingEnabled(player)) {
+            Location location = block.getLocation();
+
+            if (hookRegistry.isAvailableToSit(location, player)) {
+                if (isWorldDenied(location.getWorld())) {
+                    return;
+                }
+
+                if (block.getRelative(BlockFace.UP).getType() != Material.AIR) {
+                    return;
+                }
+
+                if (chairDataRegistry.isLocationUsed(location)) {
+                    return;
+                }
+
+                chairDataRegistry.removeRegistry(player);
+                chairDataRegistry.createAndAddRegistry(player, block);
+            }
+        }
+    }
+
+    @Override
+    public void crawl(Player player) {
+        Location location = player.getLocation();
+        if (isWorldDenied(location.getWorld())) {
+            return;
+        }
+
+        if (!hookRegistry.isAvailableToCrawl(player)) {
+            return;
+        }
+
+        Block block = location.getBlock();
+
+        if (materialChecker.isSlab(block) ||
+                materialChecker.isStair(block) ||
+                materialChecker.isStair(
+                        location.subtract(0, 1, 0).getBlock()
+                )) {
+            return;
+        }
+
+        if (userToggleSeatingManager.hasSeatingEnabled(player)) {
+            if (crawlDataRegistry.removeRegistry(player) == null) {
+                crawlDataRegistry.createAndAddRegistry(player, null);
+            }
+        }
+    }
+
+}
