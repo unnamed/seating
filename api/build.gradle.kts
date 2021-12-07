cadfc297709cd56157719c43ca6b2935a900b18d
pixeldev <amiranda8695@gmail.com>
2021-12-07 17:27:18 -0600

    deps: use 1.16 as base (to access 1.13+ methods)

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     api("org.jetbrains:annotations:22.0.0")
     api("team.unnamed.common:commons-bukkit:2.0.0")
-    compileOnly("org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.16.5-R0.1-SNAPSHOT")
 }
